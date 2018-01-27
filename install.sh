ccadd05f4e135e1dfcd31767e88f705683f26845
Zac McCormick <zac.mccormick@gmail.com>
2018-01-27 10:31:30 -0500

    Update script

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 ENV_PATH=$HOME/Dropbox/stuff/env
 
-ln -sf $ENV_PATH ~/.envfiles
-ln -sf $ENV_PATH/.zshrc ~/.zshrc
-ln -sf $ENV_PATH/.aliases ~/.aliases
-ln -sf $ENV_PATH/.functions ~/.functions
-ln -sf $ENV_PATH/.gemrc ~/.gemrc
-ln -sf $ENV_PATH/.gitconfig ~/.gitconfig
-ln -sf $ENV_PATH/.gitignore ~/.gitignore
-ln -sf $ENV_PATH/.irbrc ~/.irbrc
+ln -sf $ENV_PATH $HOME/.envfiles
+ln -sf $ENV_PATH/.zshrc $HOME/.zshrc
+ln -sf $ENV_PATH/.aliases $HOME/.aliases
+ln -sf $ENV_PATH/.functions $HOME/.functions
+ln -sf $ENV_PATH/.gemrc $HOME/.gemrc
+ln -sf $ENV_PATH/.gitconfig $HOME/.gitconfig
+ln -sf $ENV_PATH/.gitignore $HOME/.gitignore
+ln -sf $ENV_PATH/.irbrc $HOME/.irbrc
 
-ln -sf $HOME/Dropbox/dev/oh-my-zsh .oh-my-zsh
+ln -sf $HOME/Dropbox/dev/oh-my-zsh $HOME/.oh-my-zsh
 
-ln -sf $HOME/Dropbox/dev/vim/.vim .vim
-ln -sf $HOME/Dropbox/dev/vim/.vimrc .vimrc
+ln -sf $HOME/Dropbox/dev/vim/.vim $HOME/.vim
+ln -sf $HOME/Dropbox/dev/vim/.vimrc $HOME/.vimrc
 
-ln -sf $HOME/Dropbox/code ~/code
-ln -sf $HOME/Dropbox/dev ~/dev
-ln -sf $HOME/Dropbox/stuff ~/stuff
-ln -sf $HOME/Dropbox/temp ~/temp
+ln -sf $HOME/Dropbox/code $HOME/code
+ln -sf $HOME/Dropbox/dev $HOME/dev
+ln -sf $HOME/Dropbox/stuff $HOME/stuff
+ln -sf $HOME/Dropbox/temp $HOME/temp
